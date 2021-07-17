2125d81acd89dfe8f5b6a470159f1ff294e3d7ee
Shane Poppleton <shane@bjja.com.au>
2021-07-17 18:51:40 +1000

    Update config path

--- src/TwilioServiceProvider.php
+++ src/TwilioServiceProvider.php
@@ -8,12 +8,12 @@ class TwilioServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([__DIR__.'/../config/twilio.php' => config_path('twilio.php')], 'twilio-config');
+        $this->publishes([__DIR__.'/../config/twilio.php' => config_path('twilio.php')], 'twilio');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/twilio.php', 'twilio-config');
+        $this->mergeConfigFrom(__DIR__.'/../config/twilio.php', 'twilio');
         $this->app->bind('twilio', fn($app) => new TwilioService());
     }
 }
